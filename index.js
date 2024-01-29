f9ce4284d6deb7a0ae03a50f355f988376a2546f
Miad Hasan <mdhasan9.sy@gmail.com>
2024-01-29 18:56:12 +0600

    challenge completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -11,7 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),  async (req, res) => {
+  try {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } catch (e) {
+    res.send(e);
+  }
+})
 
 
 const port = process.env.PORT || 3000;
